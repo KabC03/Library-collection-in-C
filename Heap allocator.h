1a35c62b550121499a12626af3180735edecf8ed
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-27 01:51:44 +1000

    Create Heap allocator.h

---
+++ Heap allocator.h
@@ -0,0 +1,19 @@
+//Generic vectors in C - uses uint8_t to index with bytes directly
+
+#ifndef HEAP_ALLOCATOR_H 
+#define HEAP_ALLOCATOR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+
+
+
+
+
+
+#endif // HEAP_ALLOCATOR_H
+
+
