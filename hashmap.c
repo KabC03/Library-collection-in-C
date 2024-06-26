c7f70de6b20dfa14e51b1f90249c6cf350f34586
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-26 21:05:32 +1000

    Update hashmap.c

--- hashmap.c
+++ hashmap.c
@@ -76,6 +76,10 @@ bool hashmap_initialise(HashMap *const hashmap, size_t keySize, size_t valueSize
         for(size_t i = 0; i < initialTableSize; i++) {
             //vector_get_index can return null ptr but map_LL will notice it
             if(map_LL_initialise((MapList*)vector_get_index(&(hashmap->mapListNodes), i)) == false) {
+                for(int j = 0; j < i; i++) {
+                    map_LL_destroy((MapList*)vector_get_index(&(hashmap->mapListNodes), i));
+                }
+                
                 vector_destroy(&(hashmap->mapListNodes));
             }
         
