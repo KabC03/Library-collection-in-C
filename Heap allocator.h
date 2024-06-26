f9509810666cce22a09da1552d5781e7f8d71c9f
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-27 02:08:38 +1000

    Update Heap allocator.h

--- Heap allocator.h
+++ Heap allocator.h
@@ -1,4 +1,4 @@
-//Generic vectors in C - uses uint8_t to index with bytes directly
+//Custom malloc and free function
 
 #ifndef HEAP_ALLOCATOR_H 
 #define HEAP_ALLOCATOR_H
@@ -8,11 +8,23 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
+#include <sys/mman.h>
 
+typedef struct FreeMemoryNode {
 
+    size_t blockSize;            //Size of the block
+    struct FreeMemoryNode *next; //Next block
 
+} FreeMemoryNode;
+typedef struct Heap { //Do this to allow for multiple heaps
 
+    size_t totalSize;
+    struct MemoryNode *memoryNode; //A memory node contained within the heap
 
+} Heap;
+
+void *heap_allocate(size_t size);
+bool heap_free(void *ptr);
 
 #endif // HEAP_ALLOCATOR_H
 
