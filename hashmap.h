4f9b3157eb87dcb7095326710a3821e8b7c7bcad
Kabir <167163594+KabC03@users.noreply.github.com>
2024-06-27 00:20:10 +1000

    Update hashmap.h

--- hashmap.h
+++ hashmap.h
@@ -27,7 +27,8 @@ bool hashmap_print(HashMap *const hashmap);
 bool hashmap_initialise(HashMap *const hashmap, size_t keySize, size_t valueSize, size_t TableSize);
 bool hashmap_insert(HashMap *const hashmap, void *const key, void *const value);
 bool hashmap_get_value(HashMap *const hashmap, void *const key, const void **valueOut);
-
+bool hashmap_remove(HashMap *const hashmap, void *const key);
+bool hashmap_destroy(HashMap *const hashmap);
 
 #endif // HASHMAP_H
 
