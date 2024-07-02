0739066b33fe4cf23b2c5a3437a0527fa1ab766c
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-03 07:21:57 +1000

    Create string hashmap.h

---
+++ string hashmap.h
@@ -0,0 +1,26 @@
+
+//Hashmap for strings (since the other hashmap library doesnt work with them)
+
+#ifndef STRING_HASHMAP_H
+#define STRING_HASHMAP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include "vector.h"
+#include "datastructures.h"
+
+
+typedef struct StringHashmap{
+
+    Vector stringMapListNode;
+
+} StringHashmap;
+
+bool string_hashmap_initialise(StringHashmap *stringHashmap, size_t initialHashmapSize);
+
+
+
+#endif // STRING_HASHMAP_H
