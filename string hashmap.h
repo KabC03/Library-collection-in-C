2b0ebc353f40cd1c5e37f8f019fdcd073cb379b5
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-04 06:41:30 +1000

    Update and rename string hashmap.h (WIP) to string hashmap.h

---
+++ string hashmap.h
@@ -0,0 +1,33 @@
+
+//Hashmap for strings (since the other hashmap library doesnt work with them)
+//Note - will add deletion functions at some point
+//Also note - this is designed for strings but also works with other datatypes - but the static map is more efficient
+
+#ifndef STRING_HASHMAP_H
+#define STRING_HASHMAP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include "vector.h"
+#include "datastructures.h"
+
+
+typedef struct StringHashmap{
+
+    Vector stringMapListNode;
+
+} StringHashmap;
+
+
+bool string_hashmap_print(StringHashmap *stringHashmap);
+bool string_hashmap_initialise(StringHashmap *stringHashmap, size_t initialHashmapSize);
+bool string_hashmap_set(StringHashmap *stringHashmap, void *key, size_t keySize, void *value, size_t valueSize);
+const void *string_hashmap_get_value(StringHashmap *stringHashmap, void *key, size_t keySize);
+
+#endif // STRING_HASHMAP_H
+
+
+
