ada50f668b019cf6f43dd3cca5814a203aa71e02
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-14 12:48:22 +1000

    Create dynamic strings.h

---
+++ dynamic strings.h
@@ -0,0 +1,25 @@
+//Memory safe dynamic strings in C - meant to be used so no malloc have to appear in the code in relation to a string
+
+#ifndef DYNAMIC_STRING_H 
+#define DYNAMIC_STRING_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+
+typedef struct DynamicString {
+
+    char *data;
+    size_t size; //Size including NULL terminator
+
+} DynamicString;
+
+
+bool dynamic_string_initialise(DynamicString *dynamicString);
+bool dynamic_string_set(DynamicString *dynamicString, char *inputString);
+const char *dynamic_string_read(DynamicString *dynamicString);
+bool dynamic_string_concatanate(DynamicString *dynamicString, char *inputString);
+
+#endif // DYNAMIC_STRING_H
