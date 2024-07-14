9bcf02d2f2a0daa1a105a12714fffa552487cf49
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-14 13:03:28 +1000

    Create bitmaps.h

---
+++ bitmaps.h
@@ -0,0 +1,20 @@
+
+//Generic vectors in C - uses uint8_t to index with bytes directly
+
+#ifndef BITMAP_H 
+#define BITMAP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include "vector.h"
+
+#endif // BITMAP_H
+
+
+
+
+
+
