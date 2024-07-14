cc1a39e98f3637b7ffd9be53e38384ffd62627c3
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-14 13:07:34 +1000

    Update bitmaps.h

--- bitmaps.h
+++ bitmaps.h
@@ -1,5 +1,4 @@
-
-//Generic vectors in C - uses uint8_t to index with bytes directly
+//A basic bitmap library in C
 
 #ifndef BITMAP_H 
 #define BITMAP_H
@@ -11,8 +10,42 @@
 #include <stdint.h>
 #include "vector.h"
 
-#endif // BITMAP_H
 
+#pragma pack(push, 1)
+
+typedef struct BMPHeader {
+
+    uint16_t fileType;
+    uint32_t fileSize;
+    uint16_t res1;
+    uint16_t res2;
+    uint32_t dataOffset;
+
+} BMPHeader;
+
+
+typedef struct BMPMetadata {
+
+    uint32_t headerSize;
+    int32_t imageWidth;
+    int32_t imageHeight;
+    uint16_t numberOfPlanes;
+    uint16_t bitsPerPixel;
+    uint32_t compressionType;
+    uint32_t imageFileSize;
+    int32_t xRes;
+    int32_t yRes;
+    uint32_t numberOfColours;
+    uint32_t importantColours;
+
+} BMPMetadata;
+
+
+#pragma pack(pop)
+
+
+
+#endif // BITMAP_H
 
 
 
