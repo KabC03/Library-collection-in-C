2830ccd934b0d58eed4ef0a1e60ed4cdb4ad8bb5
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-19 05:57:41 +1000

    Update dynamic strings.c

--- dynamic strings.c
+++ dynamic strings.c
@@ -1,33 +1,140 @@
-//Memory safe dynamic strings in C - meant to be used so no malloc have to appear in the code in relation to a string
-
-#ifndef DYNAMIC_STRING_H 
-#define DYNAMIC_STRING_H
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdint.h>
-
-typedef struct DynamicString {
-
-    char *data;
-    size_t size; //Size including NULL terminator
-
-} DynamicString;
+#include "dynamic string.h"
 
 
-RETURN_CODE dynamic_string_initialise(DynamicString *dynamicString);
-RETURN_CODE dynamic_string_set(DynamicString *dynamicString, char *inputString);
-const char *dynamic_string_read(DynamicString *dynamicString);
-RETURN_CODE dynamic_string_concatanate(DynamicString *dynamicString, char *inputString);
 
-#endif // DYNAMIC_STRING_H
-      
-      
-      
-      
-      
-      
-      
-      
+
+
+
+
+
+/**
+ * dynamic_string_initialise 
+ * ===============================================
+ * Brief: Initialise a dynamic string 
+ * 
+ * Param: *dynamicString - string of interest
+ * 
+ * Return: RETURN_CODE - T/F depending on if addition was successful
+ * 
+ */
+RETURN_CODE dynamic_string_initialise(DynamicString *dynamicString) {
+
+    if(dynamicString == NULL) {
+        return _INVALID_ARG_PASS_;
+    } else {
+        dynamicString->size = 0;
+        dynamicString->data = NULL;
+    }
+
+    return _SUCCESS_;
+}
+
+
+
+
+
+/**
+ * dynamic_string_initialise 
+ * ===============================================
+ * Brief: Set or reset a dynamic string 
+ * 
+ * Param: *dynamicString - string of interest
+ *        *inputString - String to copy from
+ * 
+ * Return: RETURN_CODE - T/F depending on if addition was successful
+ * 
+ */
+RETURN_CODE dynamic_string_set(DynamicString *dynamicString, char *inputString) {
+
+    if(dynamicString == NULL || inputString == NULL) {
+        return _INVALID_ARG_PASS_;
+    } else {
+
+        char *newPtr = realloc(dynamicString->data, strlen(inputString) + 1);
+        if(newPtr == NULL) { //Realloc returns NULL if block is unchaged
+            return _ALLOC_FAILURE_;
+        } else {
+            dynamicString->data = newPtr;
+        }
+
+
+        dynamicString->size = strlen(inputString) + 1;
+        strncpy(dynamicString->data, inputString, dynamicString->size); 
+    }
+
+    return _SUCCESS_;
+}
+
+
+
+
+/**
+ * dynamic_string_read 
+ * ===============================================s
+ * Brief: Read from a dynamic string 
+ * 
+ * Param: *dynamicString - string of interest
+ *        *inputString - String to copy from
+ * 
+ * Return: Pointer to the dynamic string 
+ * 
+ */
+const char *dynamic_string_read(DynamicString *dynamicString) {
+
+    if(dynamicString == NULL) {
+        return _INVALID_ARG_PASS_;
+    } else {
+        return dynamicString->data; 
+    }
+
+
+    return _INTERNAL_ERROR_; //Put this here so the compiler doesnt complain 
+}
+
+
+
+
+
+/**
+ * dynamic_string_concatanate 
+ * ===============================================s
+ * Brief: Concatanate a dynamic string with another 
+ * 
+ * Param: *dynamicString - string of interest
+ *        *inputString - String to add 
+ * 
+ * Return: Pointer to the dynamic string 
+ * 
+ */
+RETURN_CODE dynamic_string_concatanate(DynamicString *dynamicString, char *inputString) {
+
+    if(dynamicString == NULL || inputString == NULL) {
+        return _INVALID_ARG_PASS_;
+    } else {
+
+        char *newPtr = realloc(dynamicString->data, dynamicString->size + strlen(inputString) + 1);
+        if(newPtr == NULL) {
+            return _ALLOC_FAILURE_;
+        } else {
+
+            dynamicString->data = newPtr;
+            dynamicString->size += strlen(inputString) + 1;
+            strcat(dynamicString->data, inputString);
+        }
+
+    }
+
+    return _SUCCESS_;
+}
+
+/*
+TODO: add later
+- Compare strings (stcmp)
+- And so on
+*/
+
+
+
+
+
+
