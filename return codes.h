2d1c9290d0ad4d086a8531077033d1a2554cc99d
Kabir <167163594+KabC03@users.noreply.github.com>
2024-07-19 05:58:05 +1000

    Update return codes.h

--- return codes.h
+++ return codes.h
@@ -6,23 +6,19 @@
 typedef enum RETURN_CODE {
 
 
-    _INVALID_ARG_PASS,       //Invalid argument (e.g NULL ptr)
+    _INVALID_ARG_PASS_,      //Invalid argument (e.g NULL ptr)
     _INVALID_MODE_PASS_,     //Used for functions that might accept modes based on a char
 
-    
-
     _ALLOC_FAILURE_,         //If dynamic memory allocation fails
-
-
 
     _FILE_NOT_OPENED_,       //If a file was not opened
     _FILE_NOT_CLOSED_,       //If a file was not closed
 
-
-
     _ALREADY_EXISTS_,        //If an item already exists and trying to create another
+
     _KEY_ERROR_,             //Key error (e.g key does not exist in dict)
     _VALUE_ERROR_,           //Value error (e.g value not found in array or dict)
+
     _INDEX_ERROR_,           //Index error (e.g access beyond array bounds)
 
 
