be3dabd885fc59957b72c91f3b13e3d7799458ab
Kabir Chandra <kabirchandra@Kabirs-MacBook-Pro.local>
2024-08-13 08:23:45 +1000

    update

--- MLP.c
+++ MLP.c
@@ -1,5 +1,24 @@
 #include "MLP.h"
+#define MLP_RAND_RANGE 0.1
+#define MLP_RAND_MIN 0.001
 #define square(value) (value * value)
+#define HERE printf("HERE\n");
+#define BYTE_SIZE 255
+#define BYTES_PER_PIXEL 3
+#define MLP_activate(arg1, result) MLP_ReLu(arg1, result)
+
+
+
+//Data for the hidden layer
+typedef struct NetworkLayer {
+
+    Matrix weight;              //Weight for each neuron
+    Matrix bias;                //Bias
+    Matrix preActivationOutput; //Store preactivation output for back propagation
+    Matrix output;              //Output of the layer (after activation, bias, etc)
+
+} NetworkLayer;
+
 
 /**
  * MLP_randomise
@@ -20,7 +39,7 @@ bool MLP_randomise(Matrix *const matrix, float range, float min) {
         return false;
     } else {
 
-        srand(time(NULL));
+
         for(size_t i = 0; i < (matrix->cols * matrix->rows); i++) {
             matrix->data[(matrix->dataSize) * i] = (min + (range * (float)rand())/RAND_MAX);
         }
@@ -29,7 +48,33 @@ bool MLP_randomise(Matrix *const matrix, float range, float min) {
     return true;
 }
 
+/**
+ * MLP_normalise
+ * ===============================================
+ * Brief: Normalise a matrix by a value
+ * 
+ * Param: *matrix - matrix of interest
+ *        value - value to normalise to
+ * 
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+bool MLP_normalise(Matrix *const matrix, float value) {
 
+    if(matrix == NULL) {
+        return false;
+    } else if(matrix->data == NULL) {
+        return false;
+    } else {
+
+
+        for(size_t i = 0; i < (matrix->cols * matrix->rows); i++) {
+            matrix->data[(matrix->dataSize) * i] /= value;
+        }
+
+    }
+    return true;
+}
 
 
 /**
@@ -143,6 +188,256 @@ bool MLP_ReLu_gradient(Matrix *const arg1, Matrix *const result) {
     }
     return true;
 }
+
+
+
+
+
+
+/**
+ * MLP_instantiate_network
+ * ===============================================
+ * Brief: Create a MLP network
+ * 
+ * Param: *network - Network to be instantiated
+ *        numberOfLayers - Number of layers to network (When = 2 this means there is a direct connection from input -> output layer)
+ *        *neuronsPerLayer - Array of neurons per layer, MUST be 1 longer than the number of layers, where last number is DUPLICATED
+ *                      E.g if numberOfLayers = 2, then len(neuronsPerLayer) MUST = 3, where arr[1] == arr[2], this is NOT checked by the function for performance
+ *        
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+RETURN_CODE MLP_initialise_network(Network *network, size_t numberOfLayers, size_t *neuronsPerLayer) {
+
+    if(network == NULL || numberOfLayers < 2 || neuronsPerLayer == NULL) { //2 layers means inputs are directly connected to output
+        return _INVALID_ARG_PASS_;
+
+    } else {
+
+        
+        if(vector_initialise(&(network->networkLayers), sizeof(NetworkLayer)) == false) { //Vector of matricies
+            return _INTERNAL_ERROR_;
+        }
+        if(vector_resize(&(network->networkLayers), numberOfLayers) == false) {
+            return _INTERNAL_ERROR_;
+        }
+        for(size_t i = 0; i < numberOfLayers; i++) { //NOTE: since going to i < layers, MUST make sure that last layer is DUPLICATED, e.g if number layers = 2, per layer = {x,y,z}
+
+            NetworkLayer currentNetworkLayer;
+
+
+            //Preactivation output: Rows - Number of output neurons, Cols - 1
+            if(matrix_2D_initialise(&(currentNetworkLayer.preActivationOutput), neuronsPerLayer[i + 1], 1, sizeof(float)) == false) {
+                return _INTERNAL_ERROR_;
+            }
+            //Activation output: Rows - Number of output neurons, Cols - 1
+            if(matrix_2D_initialise(&(currentNetworkLayer.output), neuronsPerLayer[i + 1], 1, sizeof(float)) == false) {
+                return _INTERNAL_ERROR_;
+            }
+
+
+            //Weights: Rows - Number of output neurons, Cols - Number of current neurons
+            if(matrix_2D_initialise(&(currentNetworkLayer.weight), neuronsPerLayer[i + 1], neuronsPerLayer[i], sizeof(float)) == false) {
+                return _INTERNAL_ERROR_;
+            }
+            if(MLP_randomise(&(currentNetworkLayer.weight), MLP_RAND_RANGE, MLP_RAND_MIN) == false) { //Randomise weights
+                return _INTERNAL_ERROR_;
+            }
+
+
+            //Biases: Rows - Number of output neurons, Cols - 1
+            if(matrix_2D_initialise(&(currentNetworkLayer.bias), neuronsPerLayer[i + 1], 1, sizeof(float)) == false) {
+                return _INTERNAL_ERROR_;
+            }
+            if(MLP_randomise(&(currentNetworkLayer.bias), MLP_RAND_RANGE, MLP_RAND_MIN) == false) { //Randomise weights
+                return _INTERNAL_ERROR_;
+            }
+
+
+
+
+            //Insert layer to network
+            if(vector_insert_index(&(network->networkLayers), i, &currentNetworkLayer) == false) {
+                return _INTERNAL_ERROR_;
+            }
+        }
+
+    }
+
+    return _SUCCESS_;
+}
+
+
+
+
+
+/**
+ * MLP_evaluate_input
+ * ===============================================
+ * Brief: Evaluate a network output (requires inputs to be set) - ASSUMES R-G-B in GREYSCALE format
+ * 
+ * Param: *network - Network of interest
+ *        
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+RETURN_CODE MLP_evaluate_input(Network *network, Vector *input) {
+
+    if(network == NULL || input == NULL) {
+        return _INVALID_ARG_PASS_;
+
+    } else {
+
+        if(vector_get_length(input) == -1 || vector_get_length(input) == 0) {
+            return _INVALID_ARG_PASS_;
+        }
+
+
+        //Input data to network
+        /*
+        TODO:
+        - Convert input vector data to floats
+        - Normalise matrix
+        */
+
+
+        //Run model
+        /*
+        TODO:
+        for layer in layers:
+        
+        preActivatedOutput = [weights]*[input] + [bias]; //STORE THIS
+        output = act(preActivatedOutput); //STORE THIS
+        input = output;
+
+        */
+
+       //This is VERY inefficient - should optimise it later
+        Matrix inputToInputLayer;
+        Vector inputAsFloat;
+        if(vector_initialise(&inputAsFloat, sizeof(float)) == false) {
+            return _INTERNAL_ERROR_;
+        }
+        //Convert uint8 to float
+        for(size_t i = 0; i < vector_get_length(input) + 1; i++) {
+            const uint8_t *currentInt = vector_get_index(input, i); //Get the bottom 8 bits
+
+            if(currentInt == NULL) {
+                return _INTERNAL_ERROR_;
+            }
+
+            float current = (float)(*currentInt); //Have to do it in one line otherwise a buffer overflow occurs
+
+            if(vector_quick_append(&inputAsFloat, &(current), 1) == false) {
+                return _INTERNAL_ERROR_;
+            }
+        }
+
+
+
+
+
+        //Assign to new matrix
+        if(matrix_2D_initialise(&inputToInputLayer, vector_get_length(input) + 1, 1, sizeof(float)) == false) {
+            return _INTERNAL_ERROR_;
+        }
+        if(MLP_normalise(&inputToInputLayer, BYTE_SIZE) == false) {
+            return _INTERNAL_ERROR_;
+        }
+        if(matrix_2D_set(&inputToInputLayer, vector_get_length(&inputAsFloat) + 1, 1, inputAsFloat.data, sizeof(float)) == false) {
+            return _INTERNAL_ERROR_;
+        }
+
+        /*
+        printf("Rows: %zu, Cols: %zu\n",inputToInputLayer.rows, inputToInputLayer.cols);
+        if(matrix_2D_print(&inputToInputLayer) == false) {
+            return _INTERNAL_ERROR_;
+        }
+        printf("\n");
+        */
+
+        Matrix *inputToLayer = &inputToInputLayer;
+        size_t numberOfLayers = vector_get_length(&(network->networkLayers));
+        for(size_t i = 0; i < numberOfLayers; i++) {
+
+            NetworkLayer *currentLayer = (NetworkLayer*)vector_get_index(&(network->networkLayers), i);
+            if(currentLayer == NULL) {
+                return _INTERNAL_ERROR_;
+            }
+
+            //preActivatedOutput = [weights]*[input] + [bias]; //STORE THIS
+            if(matrix_2D_multiply(&(currentLayer->preActivationOutput), &(currentLayer->weight), inputToLayer) == false) { //ISSUE HERE
+                return _INTERNAL_ERROR_;
+            }
+            if(matrix_2D_add(&(currentLayer->preActivationOutput), &(currentLayer->preActivationOutput), &(currentLayer->bias)) == false) {
+                return _INTERNAL_ERROR_;
+            }
+
+            //output = act(preActivatedOutput); //STORE THIS
+            if(MLP_activate(&(currentLayer->preActivationOutput), &(currentLayer->output)) == false) {
+                return _INTERNAL_ERROR_;
+            }
+
+            //input = output;
+            inputToLayer = &(currentLayer->output);
+
+
+
+            /*
+            printf("Layer: %zu\n",i);
+            if(matrix_2D_print(&(currentLayer->output)) == false) {
+                return _INTERNAL_ERROR_;
+            }
+            printf("\n");
+            */
+        }
+
+
+
+    }
+
+    return _SUCCESS_;
+}
+
+
+
+
+
+/**
+ * MLP_print_output
+ * ===============================================
+ * Brief: Print the output matrix at the output layer
+ * 
+ * Param: *network - Network of interest
+ *        
+ * Return: bool - T/F depending on if initialisation was successful
+ * 
+ */
+RETURN_CODE MLP_print_output(Network *network) {
+
+    if(network == NULL) {
+        return _INVALID_ARG_PASS_;
+
+    } else {
+
+        NetworkLayer *outputLayer = (NetworkLayer*)vector_get_index(&(network->networkLayers), vector_get_length(&(network->networkLayers)));
+
+        if(matrix_2D_print(&(outputLayer->output)) == false) {
+            return _INTERNAL_ERROR_;
+        }
+    }
+
+    return _SUCCESS_;
+}
+
+
+
+
+
+
+
+
+
 
 
 
--- MLP.h
+++ MLP.h
@@ -10,13 +10,33 @@
 #include <stdint.h>
 #include <time.h>
 #include <limits.h>
-#include "2Dmatrix.h"
+#include "2DMatrix.h"
+#include "vector.h"
+#include "return codes.h"
+
+typedef struct Network {
+
+    Vector networkLayers; //Layer data (metadata) - note network output is stored in the last layers output matrix
+
+
+} Network;
+
+
+
+
 
 bool MLP_randomise(Matrix *const matrix, float range, float min);
+bool MLP_normalise(Matrix *const matrix, float value);
 bool MLP_mean_square_error(Matrix *const matrix, float expectedValue ,float *output);
 
 bool MLP_ReLu(Matrix *const arg1, Matrix *const result);
 bool MLP_ReLu_gradient(Matrix *const arg1, Matrix *const result);
+
+
+
+RETURN_CODE MLP_initialise_network(Network *network, size_t numberOfLayers, size_t *neuronsPerLayer);
+RETURN_CODE MLP_evaluate_input(Network *network, Vector *input);
+RETURN_CODE MLP_print_output(Network *network);
 
 #endif // MLP_H
 
