a33566650241a57b6e40e834bcc62d1fad4ca759
Kabir Chandra <kabirchandra@Kabirs-MacBook-Pro.local>
2024-08-13 04:41:01 +1000

    update

--- bitmaps.h
+++ bitmaps.h
@@ -9,7 +9,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
-#include "vector.h"
+#include "vector.h" 
 #include "return codes.h"
 
 
@@ -54,9 +54,12 @@ typedef struct BitmapImage {
 
 
 RETURN_CODE bitmap_enstantiate(char *bitmapPath, BitmapImage *bitmapImageOutput);
+RETURN_CODE bitmap_generate_image_24(BitmapImage *outputImage, uint8_t red, uint8_t green, uint8_t blue, size_t xRes, size_t yRes);
 RETURN_CODE bitmap_greyscale(BitmapImage *bitmapImage);
 RETURN_CODE bitmap_reconstruct_image(BitmapImage *bitmapImage, char *imageName);
 RETURN_CODE bitmap_destroy(BitmapImage *bitmapImage);
+RETURN_CODE bitmap_return_metadata(BitmapImage *bitmapImage, BitmapMetadata *outputMetadata);
+
 
 #endif // BITMAP_H
 
