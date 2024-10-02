6c5a11feb050df98217b343c0b29488699d575e8
Kabir Chandra <kabirchandra@Kabirs-MacBook-Pro.local>
2024-10-03 04:36:21 +1000

    update

--- lib/main.c
+++ lib/main.c
@@ -2,15 +2,6 @@
 
 int main(void) {
 
-    List l1;
-    list_init(&l1, sizeof(int));
-    
-    for(int i = 0; i < 1000000; i++) {
-        list_append(&l1, &i);
-    }
-
-    list_disp(&l1, list_print_integer);
-    printf("%d\n", *(int*)(list_peak_back(&l1)->data));
 
 
     return 0;
