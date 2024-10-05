b38d2a2f6ef75bd7cc6a09fd691d62ebb2769f29
Kabir Chandra <kabirchandra@Kabirs-MacBook-Pro.local>
2024-10-06 08:03:46 +1100

    update

--- lib/hashmap.c
+++ lib/hashmap.c
@@ -183,7 +183,7 @@ bool hashmap_resize(Hashmap *hashmap, size_t size, size_t (*hashmapFunction)(uin
         
         Node **current = &(list->head);
         Node *temp = list->head;
-        for(size_t j = 0; j < list->size; j++) {
+        for(size_t j = 0; j < *current != NULL; j++) {
             EntryData *entryData = (EntryData*)((*current)->data);
 
             //Insertion algoritm
